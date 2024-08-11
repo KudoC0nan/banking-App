aff4d768f160854ee5a9616fc05ebb6aa6f5ba70
KudoC0nan <patilshreyasvjti@gmail.com>
2024-08-11 23:58:46 +0530

    Added Authform and updated tsconfig

--- components/Siderbar.tsx
+++ components/Siderbar.tsx
@@ -13,7 +13,7 @@ const Siderbar = ({ user } : SiderbarProps) => {
   return (
     <section className='sidebar'>
         <nav className='flex flex-col gap-4'>
-            <Link href='/' className='mb-12 cursor-pointer items-center gap-2 flex'>
+            <Link href='/' className='mb-12 cursor-pointer items-center gap-2 flex px-4'>
                 <Image src='/icons/logo.svg' width={34} height={34} alt='Nika Logo' className='size-[24px] max-xl:size-14' />
             <h1 className='sidebar-logo'>Nika</h1>
             </Link>
