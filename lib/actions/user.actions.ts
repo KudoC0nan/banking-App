afcd56467237c5e1171d803419939cb10c27d863
KudoC0nan <patilshreyasvjti@gmail.com>
2024-08-13 12:09:18 +0530

    added .env anf appwrite server side file

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,17 @@
+'use server'
+
+export const signIn = async (userData: SignUpParams ) => {
+    try {
+        
+    } catch (error) {
+        console.error('Error', error)
+    }
+}
+
+export const signUp = async (userData: SignUpParams ) => {
+    try {
+        
+    } catch (error) {
+        console.error('Error', error)
+    }
+}
