3b5b4b515464218942f4a3be3c372a1d873aea11
KudoC0nan <patilshreyasvjti@gmail.com>
2024-08-16 15:17:26 +0530

    modified plaid link and add dwolla action

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -55,9 +55,21 @@ const AuthForm = ({ type }: { type: string }) => {
 
     try {
       // Sign up with App write and create plaid token
+      const userData = {
+        firstName: data.firstName!,
+        lastName: data.lastName!,
+        address1: data.address1!,
+        city: data.city!,
+        state: data.state!,
+        postalCode: data.postalCode!,
+        dateOfBirth: data.dateOfBirth!,
+        ssn: data.ssn!,
+        email: data.email,
+        password: data.password,
+      }
 
       if (type === 'sign-up') {
-        const newUser = await signUp(data)
+        const newUser = await signUp(userData)
         setUser(newUser);
       }
 
@@ -99,13 +111,13 @@ const AuthForm = ({ type }: { type: string }) => {
           </div>
 
         </header>
-        {/* {user ? ( */}
+        {user ? (
           <div className='flex flex-col gap-4'>
             
             <PlaidLink user={user} variant='primary' />
 
           </div>
-        {/* ) : ( */}
+        ) : (
           <>
             <Form {...form}>
               <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -161,7 +173,7 @@ const AuthForm = ({ type }: { type: string }) => {
             </footer>
 
           </>
-        {/* )} */}
+        )}
     </section>
   )
 }
--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -1,8 +1,8 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import { Button } from './ui/button'
 import { PlaidLinkOnSuccess, PlaidLinkOptions, usePlaidLink } from 'react-plaid-link'
-import { StyledString } from 'next/dist/build/swc'
 import { useRouter } from 'next/navigation'
+import { createLinkToken, exchangePublicToken } from '@/lib/actions/user.actions'
 
 const PlaidLink = ({ user, variant } : PlaidLinkProps) => {
 
@@ -11,22 +11,21 @@ const PlaidLink = ({ user, variant } : PlaidLinkProps) => {
 
     useEffect(() => {
       const getLinkToken = async () => {
-        // const data = await createLinkToken(user)
+        const data = await createLinkToken(user)
 
-        // setToken(data?.linkToken)
+        setToken(data?.linkToken)
       }
-    
-      return () => {
-        getLinkToken()
-      }
-    }, [])
+        
+      getLinkToken()
+
+    }, [user])
     
 
     const onSuccess = useCallback<PlaidLinkOnSuccess>(async function (public_token : string) {
-        // await exchangePublicToken({ 
-        //     public_token: public_token,
-        //     user,
-        // })
+        await exchangePublicToken({ 
+            publicToken: public_token,
+            user,
+        })
 
         router.push('/')
 
